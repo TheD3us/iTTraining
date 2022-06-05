b19b9748a382a873a36d8cc9fb6d06823723adc2
Mezier <theo.mezier@gmail.com>
2022-06-05 09:10:50 +0200

    Ajout entities

---
+++ ItTraining/WebContent/it-training-angular/it-training/src/app/entities/adresse.ts
@@ -0,0 +1,11 @@
+export class Adresse{
+    public id: number;
+    public noRue: number;
+    public complement: string;
+    public typeVoie: string;
+    public nomVoie: string;
+    public cpo: string;
+    public ville: string;
+    public etage: string;
+    public appartement: number;
+}
