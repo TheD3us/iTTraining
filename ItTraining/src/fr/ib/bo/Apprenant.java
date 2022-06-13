e0a151be10e4e372fae57e836ca557130954ca6c
Aklamanor <t.cb4041@gmail.com>
2022-06-13 11:06:18 +0200

    ajout apprenant et formateur

---
+++ ItTraining/src/fr/ib/bo/Apprenant.java
@@ -0,0 +1,60 @@
+package fr.ib.bo;
+
+public class Apprenant extends Personne {
+	private boolean candidatAccepte;
+	private Formation formation;
+	private Machine machine;
+	private Entreprise entreprise;
+	
+	
+	//Construsteurs
+	public Apprenant() {
+		super();
+	}
+
+	public Apprenant(boolean candidatAccepte, Formation formation, Machine machine, Entreprise entreprise) {
+		super();
+		this.candidatAccepte = candidatAccepte;
+		this.formation = formation;
+		this.machine = machine;
+		this.entreprise = entreprise;
+	}
+
+	public boolean isCandidatAccepte() {
+		return candidatAccepte;
+	}
+
+	public void setCandidatAccepte(boolean candidatAccepte) {
+		this.candidatAccepte = candidatAccepte;
+	}
+
+	public Formation getFormation() {
+		return formation;
+	}
+
+	public void setFormation(Formation formation) {
+		this.formation = formation;
+	}
+
+	public Machine getMachine() {
+		return machine;
+	}
+
+	public void setMachine(Machine machine) {
+		this.machine = machine;
+	}
+
+	public Entreprise getEntreprise() {
+		return entreprise;
+	}
+
+	public void setEntreprise(Entreprise entreprise) {
+		this.entreprise = entreprise;
+	}
+	
+	
+	//Getters & Setters 
+	
+	
+	
+}
