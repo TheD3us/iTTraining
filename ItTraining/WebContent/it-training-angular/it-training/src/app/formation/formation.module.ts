8df309a6bf72900284ad4aff7dbe5f6f2505af92
Ouaz27 <smazalrey7@gmail.com>
2022-06-14 11:19:54 +0200

    up

--- ItTraining/WebContent/it-training-angular/it-training/src/app/formation/formation.module.ts
+++ ItTraining/WebContent/it-training-angular/it-training/src/app/formation/formation.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListFormationComponent } from './list-formation/list-formation.component';
+
 
 
 
 @NgModule({
   declarations: [
-    ListFormationComponent
-  ],
+      ],
   imports: [
     CommonModule
   ]
